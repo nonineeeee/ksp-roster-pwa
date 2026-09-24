91522280d681c6feb542a87c6f2d550574b3a825
nonineeeee <vhubji0331@gmail.com>
2026-09-24 16:11:48 +0800

    Add files via upload

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 // 部署 Apps Script Web App 後，把 /exec 完整網址貼到這裡。
-const ROSTER_API_URL = 'https://script.google.com/macros/s/AKfycbz4dE6Hts9jpWOvSYMPMiiXRpHkjmk-BdrM1LNgroGhnnfkfSXpMO58aVxwzZpRiS0s/exec';
+const ROSTER_API_URL = 'PASTE_YOUR_ROSTER_API_EXEC_URL_HERE';
 const API_TIMEOUT_MS = 25000;
