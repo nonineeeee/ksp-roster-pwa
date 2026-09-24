6b4735f7fb601668a01f6b2891fded9bfe22cc32
nonineeeee <vhubji0331@gmail.com>
2026-09-24 15:53:42 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 // 部署 Apps Script Web App 後，把 /exec 完整網址貼到這裡。
-const ROSTER_API_URL = 'PASTE_YOUR_ROSTER_API_EXEC_URL_HERE';
+const ROSTER_API_URL = 'https://script.google.com/macros/s/AKfycbwoBIz9g6DFz7q2z65R6W5aKtruDXuTSdffsZ4fptA2qaS0fPjEpMxtEuEoSgnIfvGP/exec';
 const API_TIMEOUT_MS = 25000;
