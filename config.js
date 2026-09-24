3cf28ada9c331453bb9a434ff6cca3ef7ab86716
nonineeeee <vhubji0331@gmail.com>
2026-09-24 16:49:20 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 // 將排班專用 Apps Script Web App 的完整 /exec 網址貼在下方。
-const ROSTER4_API_URL = 'PASTE_YOUR_ROSTER4_API_EXEC_URL_HERE';
+const ROSTER4_API_URL = 'https://script.google.com/macros/s/AKfycbxAhxla-CNaX48V9fBpJfdRshDd0sKKDAGWac1qHuzUKVoFnkZM41DKeHFbhJDgHq1b/exec';
 const API_TIMEOUT_MS = 25000;
